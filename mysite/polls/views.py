96f014227d9bcd7e3d3aa0150edc0379b095aa5e
Rinava <asdasd5056@gmail.com>
2021-07-29 13:59:36 -0300

    detail view changed

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
@@ -23,4 +24,15 @@ def results(request, question_id):
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/vote.html', {'question': question})
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request, 'polls/detail.html',
+            {'question': question,
+             'error_message': "Please select a choice",
+             })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
